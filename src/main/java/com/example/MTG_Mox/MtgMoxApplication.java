83ca1dc4db42e2bcd7302ae472e5d3244d149cfd
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2025-03-05 15:57:24 -0800

    First commit

---
+++ src/main/java/com/example/MTG_Mox/MtgMoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.MTG_Mox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgMoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgMoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MTG_Mox/MtgMoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MTG_Mox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgMoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
