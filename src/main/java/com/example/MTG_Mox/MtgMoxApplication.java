bb779968dfa04178ed94c9f2b990badebd907579
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2025-03-05 16:05:28 -0800

    Fix datasource issue, unable to find datasource info

--- src/main/java/com/example/MTG_Mox/MtgMoxApplication.java
+++ src/main/java/com/example/MTG_Mox/MtgMoxApplication.java
@@ -2,8 +2,9 @@ package com.example.MTG_Mox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MtgMoxApplication {
 
 	public static void main(String[] args) {
