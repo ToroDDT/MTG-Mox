ba3b269b688a9f7e6d7e82285c3d081865632b8c
ddt <142459893+ToroDDT@users.noreply.github.com>
2025-08-05 07:33:12 -0700

    Add error handling for car and commander requests information

---
+++ src/main/java/com/example/MTG_Mox/advice/ErrorDetails.java
@@ -0,0 +1,15 @@
+package com.example.MTG_Mox.advice;
+
+public class ErrorDetails {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String newMessage) {
+        message = newMessage;
+    }
+
+}
---
+++ src/main/java/com/example/MTG_Mox/advice/InvalidCommanderCardException.java
@@ -0,0 +1,5 @@
+package com.example.MTG_Mox.advice;
+
+public class InvalidCommanderCardException extends RuntimeException {
+
+}
