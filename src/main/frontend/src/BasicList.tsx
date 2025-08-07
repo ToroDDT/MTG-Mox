d3fe726b9f693fa56b7e8d8ec2ff9bcb0b8055c6
ddt <142459893+ToroDDT@users.noreply.github.com>
2025-08-07 15:55:55 -0700

    Add list component for displaying cards

---
+++ src/main/frontend/src/BasicList.tsx
@@ -0,0 +1,23 @@
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
+export default function BasicList({ card }: { card: string }) {
+	return (
+		<Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+			<nav aria-label="main mailbox folders">
+				<List>
+					<ListItem disablePadding>
+						<ListItemButton>
+							<ListItemText primary={card} />
+						</ListItemButton>
+					</ListItem>
+				</List>
+			</nav>
+			<Divider />
+		</Box>
+	);
+}
