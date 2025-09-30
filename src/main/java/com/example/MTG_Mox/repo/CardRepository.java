001e0279c3d96c17cf16a7b172ab692d70915305
David Del Toro <Davidddt1999@gmail.com>
2025-09-29 23:16:19 -0700

    Delete cards by name
    Whenever a user sends a request to delete a card, the card will first be
    searched in the database, if a card returns and the total amount of the
    card is more then one, the total will be decremented, if the card total
    is zero, it will be actually removed

--- src/main/java/com/example/MTG_Mox/repo/CardRepository.java
+++ src/main/java/com/example/MTG_Mox/repo/CardRepository.java
@@ -1,11 +1,24 @@
 package com.example.MTG_Mox.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.MTG_Mox.model.TCG.MagicCard;
 
 @Repository
 public interface CardRepository extends JpaRepository<MagicCard, Long> {
+
+  @Modifying
+  @Query("DELETE From MagicCard c where c.id = :id")
+  @Transactional
+  void deleteByCardID(@Param("id") String id);
+
+  Optional<MagicCard> findByName(String name);
 
 }
---
+++ src/main/java/com/example/MTG_Mox/service/CardsService.java
@@ -0,0 +1,31 @@
+package com.example.MTG_Mox.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.MTG_Mox.model.TCG.MagicCard;
+import com.example.MTG_Mox.repo.CardRepository;
+
+class CardsService {
+
+  private final CardRepository cardRepository;
+
+  @Autowired
+  public CardsService(CardRepository cardRepository) {
+    this.cardRepository = cardRepository;
+  }
+
+  public void deleteCardFromDeck(String name) {
+
+    MagicCard card = cardRepository.findByName(name).orElseThrow(() -> new RuntimeException("Card does not exist"));
+
+    card.setTotal(card.getTotal() - 1);
+
+    if (card.getTotal() == 0) {
+      cardRepository.deleteByCardID(card.getId());
+    } else {
+      cardRepository.save(card);
+    }
+  }
+}
