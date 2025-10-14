63b58f4e302bef94df3f86a7b94ad8328bc04c84
David Del Toro <Deltoro1999@icloud.com>
2025-10-13 21:23:26 -0700

    Add elements for displaying deck total and sideboard cards

--- src/main/frontend/src/DeckFooter.tsx
+++ src/main/frontend/src/DeckFooter.tsx
@@ -1,16 +1,11 @@
-export default function DeckFooter({
-  cardCount,
-  sideBoard,
-}: {
-  cardCount: number;
-  sideBoard: number;
-}) {
+import { DeckInformation } from './App';
+
+function DeckFooter({ deck }: { deck: DeckInformation }) {
   return (
     <div className="fixed bottom-0 left-0 w-full bg-gradient-to-r from-purple-700 to-purple-900 text-gray-100 py-2 px-4 flex justify-between items-center shadow-lg border-t border-purple-800">
       <span className="text-sm text-purple-200">
-        <strong className="text-white">{cardCount}</strong> Main Deck /
-        {sideBoard}
-        Sideboard
+        <strong className="text-white">{deck.total}</strong> Main Deck /
+        <strong className="text-white pl-1">{deck.sideBoard}</strong> Sideboard
       </span>
       <button className="bg-purple-600 hover:bg-purple-700 text-white text-sm px-3 py-1 rounded-md transition font-medium shadow-sm">
         Save Deck
@@ -18,3 +13,5 @@ export default function DeckFooter({
     </div>
   );
 }
+
+export default DeckFooter;
